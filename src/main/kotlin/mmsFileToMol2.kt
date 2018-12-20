c166393d71729829261987b270f92007e7747662
Jonathan Bisson <bjonnh@bjonnh.net>
2018-12-19 21:12:51 -0600

    Code is published under GPLv3

--- src/main/kotlin/mmsFileToMol2.kt
+++ src/main/kotlin/mmsFileToMol2.kt
@@ -1,3 +1,21 @@
+/*
+    Spinverter - Converting and working with various spin-formats
+    Copyright (C) 2018 Jonathan Bisson
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import net.nprod.spinverter.MMS.MMSFile
 import org.openscience.cdk.Atom
 import org.openscience.cdk.AtomContainer
